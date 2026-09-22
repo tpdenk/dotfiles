1e06c07913620f0785195a529aea335e642b5722
Tim Denk <tim.denk@zymtrace.com>
2026-09-22 07:36:05 +0200

    use xdg-terminal-exec

--- home/config/hypr/autostart.lua
+++ home/config/hypr/autostart.lua
@@ -2,5 +2,5 @@ local hl = hl or error("no hl")
 
 hl.on("hyprland.start", function()
     hl.exec_cmd("hyprpaper")
-    hl.exec_cmd("alacritty -e sh -c 'fastfetch ; exec $SHELL'")
+    hl.exec_cmd("xdg-terminal-exec sh -c 'fastfetch ; exec $SHELL'")
 end)
